7145afbfd1ad675d00d94ac98b93d6b28c8a0021
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-02-15 12:54:50 +0600

    input name changer

--- js/handler.js
+++ js/handler.js
@@ -1,1 +1,10 @@
-console.log('handler connected')
+ document.getElementById('btn-update-title')
+    .addEventListener('click',function (){
+        const pageTitleElement=document.getElementById('page-title');
+        pageTitleElement.innerText='Updated Page Title Text'
+    })
+
+    document.getElementById('btn-login').addEventListener('click', function(){
+    const userInfo=document.getElementById('user-info')
+    userInfo.innerText='User Logged in Successfully';
+   })
